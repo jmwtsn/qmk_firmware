6f8605a583a4503e2e0d40bc0dbb5d3f1168035f
Rock <jmwtsn@gmail.com>
2023-07-07 15:27:18 -0300

    fixed email address

--- keyboards/reviung41/keymaps/jmwtsn/keymap.c
+++ keyboards/reviung41/keymaps/jmwtsn/keymap.c
@@ -58,7 +58,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     
     case EMAIL:
         if (record->event.pressed) {
-            SEND_STRING("jmwtsn@gmail");
+            SEND_STRING("jmwtsn@gmail.com");
         } else {
         }
         break;
